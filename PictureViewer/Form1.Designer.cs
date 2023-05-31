403cbbccf2339f156b627db295d849b6b93e4341
bortek1993 <b.wojciechowski001@gmail.com>
2023-05-31 08:55:55 +0200

    Add project files.

---
+++ PictureViewer/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace PictureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pbPicture = new System.Windows.Forms.PictureBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPicture
+            // 
+            this.pbPicture.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.pbPicture.Location = new System.Drawing.Point(12, 87);
+            this.pbPicture.Name = "pbPicture";
+            this.pbPicture.Size = new System.Drawing.Size(415, 411);
+            this.pbPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbPicture.TabIndex = 0;
+            this.pbPicture.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 23);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(175, 38);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Dodaj";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(252, 23);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(175, 38);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Usuń";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 510);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.pbPicture);
+            this.Name = "Form1";
+            this.Text = "PictureViewer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pbPicture;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ PictureViewer/Form1.cs
@@ -0,0 +1,86 @@
+﻿using PictureViewer.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        private string _filePath = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if(_filePath != null)
+                btnDelete.Enabled = false;
+            
+        }
+        
+        private void SetPicture()
+        {            
+                try
+                {
+                    pbPicture.Image = Image.FromFile(_filePath);
+                    pbPicture.SizeMode = PictureBoxSizeMode.CenterImage;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Nieprawidłowy format pliku!");
+                }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            _filePath = openFileDialog.FileName;
+
+            SetPicture();
+            btnDelete.Enabled = true;
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            pbPicture.Image = null;
+            btnDelete.Enabled = false;
+
+        }
+        public string IsLastPath
+        {
+            get
+            {
+                return Settings.Default.IsLastPath;
+            }
+            set
+            {
+                Settings.Default.IsLastPath = value;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            IsLastPath = _filePath;
+            Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _filePath = Settings.Default.IsLastPath;
+            pbPicture.Image = Image.FromFile(_filePath);
+
+        }
+    }
+}
